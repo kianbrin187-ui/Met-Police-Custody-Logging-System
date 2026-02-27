33c7bc39a66f72fdfe0712d0744f4fa80ec52f15
kianbrin187-ui <kianbrin187@gmail.com>
2026-02-27 22:43:56 +0000

    Refactor authentication functions in script.js
    Removed the existing logout and saveBooking functions, and added a login function to handle user authentication.

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycby9bpy8HdtUOgt76AZaL6kqrO8FcuTDAIDzjFKDYCsc9XpCBdyxoN1k3E219AMwjmg6/exec";
+const users = {
+"admin":"met123",
+"officer1":"police1",
+"officer2":"metpolice"
+};
 
-function logout() {
-  localStorage.removeItem("loggedIn");
-  window.location.href = "login.html";
+function login(){
+
+let id = document.getElementById("officerID").value;
+let pass = document.getElementById("password").value;
+
+if(users[id] === pass){
+window.location.href = "dashboard.html";
+}
+else{
+document.getElementById("error").innerText = "Invalid Credentials";
 }
 
-function saveBooking() {
-  fetch(WEB_APP_URL, {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({
-      type: "booking",
-      officers: document.getElementById("officers").value,
-      suspect: document.getElementById("suspect").value,
-      reason: document.getElementById("reason").value,
-      location: document.getElementById("location").value,
-      time: document.getElementById("time").value,
-      caution: document.getElementById("caution").value,
-      search: document.getElementById("search").value,
-      items: document.getElementById("items").value,
-      force: document.getElementById("force").value,
-      additional: document.getElementById("additional").value
-    })
-  })
-  .then(r => r.json())
-  .then(res => {
-    alert("Saved as " + res.arrestNumber);
-    location.reload();
-  });
 }
