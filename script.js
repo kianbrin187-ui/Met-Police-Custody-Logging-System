0e204fe8544ee5c58e06dcb14213e79218a6a9ad
kianbrin187-ui <kianbrin187@gmail.com>
2026-02-28 14:22:51 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,53 @@
-// Boot animation
-setTimeout(function(){
-document.getElementById("login-container").classList.remove("hidden");
-}, 3000);
-
-// Login
-function loginUser(){
-
 const users = {
 "admin":"met123",
 "officer1":"police1"
 };
 
+/* Boot → Login */
+setTimeout(()=>{
+document.getElementById("login-container").classList.remove("hidden");
+},3000);
+
+/* Login */
+
+function loginUser(){
+
 let user = document.getElementById("username").value;
 let pass = document.getElementById("password").value;
 
-if(users[user] && users[user] === pass){
-window.location.href = "dashboard.html";
+if(users[user] && pass === "met123"){
+
+localStorage.setItem("currentUser",user);
+window.location.href="dashboard.html";
+
 }
 else{
-document.getElementById("error").innerText = "Invalid Username or Password";
+document.getElementById("error").innerText="Invalid Credentials";
 }
 
 }
 
-// Dashboard panel loader
+/* Dashboard */
 
 function loadHome(){
-document.getElementById("main-content").innerHTML = `
-<h2>System Status: Active</h2>
-<p>Secure Network Connected</p>
-<p>Welcome Officer</p>
+
+let user = localStorage.getItem("currentUser");
+
+document.getElementById("main-content").innerHTML=`
+<h2>Hello ${user}</h2>
+<p>System Status: Secure</p>
 `;
 }
 
-function loadArrestForm(){
-document.getElementById("main-content").innerHTML = `
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc0nEq5miwEXDaBhYyFhvJC1miMQuS8uXL7X6CX0c7yisyTlg/viewform?embedded=true"
-style="width:100%; height:80vh; border:none;"></iframe>
+function loadForm(){
+
+document.getElementById("main-content").innerHTML=`
+<iframe class="form-frame"
+src="YOUR GOOGLE FORM EMBED LINK HERE">
+</iframe>
 `;
 }
 
-function loadCustody(){
-document.getElementById("main-content").innerHTML = `
-<iframe src="https://docs.google.com/spreadsheets/d/1gwpjY4qQ4U59kASsyFLvgkYKMdKCV0MOqqEBOn3cTqQ/edit"
-style="width:100%; height:80vh; border:none;"></iframe>
-`;
-}
-
-function loadLinks(){
-document.getElementById("main-content").innerHTML = `
-<iframe src="https://linktr.ee/PiggyanddoUKCR"
-style="width:100%; height:80vh; border:none;"></iframe>
-`;
+function logout(){
+window.location.href="index.html";
 }
