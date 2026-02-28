25cfada9eaf412aa7c0d81cb1db5b63532b91350
kianbrin187-ui <kianbrin187@gmail.com>
2026-02-28 11:18:33 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Boot animation
+setTimeout(function(){
+document.getElementById("login-container").classList.remove("hidden");
+}, 3000);
+
+// Login
 function loginUser(){
 
 const users = {
@@ -12,7 +18,38 @@ if(users[user] && users[user] === pass){
 window.location.href = "dashboard.html";
 }
 else{
-document.getElementById("error").innerText = "Invalid Username or Password";
+document.getElementById("error").innerText = "ACCESS DENIED";
 }
 
+}
+
+// Dashboard content loader
+
+function loadHome(){
+document.getElementById("main-content").innerHTML = `
+<h2>System Status: ACTIVE</h2>
+<p>Welcome Officer.</p>
+<p>Secure Network Connected.</p>
+`;
+}
+
+function loadArrestForm(){
+document.getElementById("main-content").innerHTML = `
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc0nEq5miwEXDaBhYyFhvJC1miMQuS8uXL7X6CX0c7yisyTlg/viewform?embedded=true"
+style="width:100%; height:80vh; border:none;"></iframe>
+`;
+}
+
+function loadCustody(){
+document.getElementById("main-content").innerHTML = `
+<iframe src="https://docs.google.com/spreadsheets/d/1gwpjY4qQ4U59kASsyFLvgkYKMdKCV0MOqqEBOn3cTqQ/edit"
+style="width:100%; height:80vh; border:none;"></iframe>
+`;
+}
+
+function loadLinks(){
+document.getElementById("main-content").innerHTML = `
+<iframe src="https://linktr.ee/PiggyanddoUKCR"
+style="width:100%; height:80vh; border:none;"></iframe>
+`;
 }
